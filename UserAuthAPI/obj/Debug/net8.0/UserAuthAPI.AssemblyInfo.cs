90f3dcf5bb2531ccdf217f2ea1b7155884b332b5
FurkanYagciiii <bdonewac@outlook.com>
2025-01-17 00:45:28 +0300

    Added database backup

--- UserAuthAPI/obj/Debug/net8.0/UserAuthAPI.AssemblyInfo.cs
+++ UserAuthAPI/obj/Debug/net8.0/UserAuthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserAuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8288c8973a4a6fae2e2e6b27a377a19204afa5e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1da877539af0efb0b8def77ab668d4de56eeded")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserAuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserAuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
