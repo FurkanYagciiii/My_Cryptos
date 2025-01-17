f5327ff8c2856021920fbd4676dd723c17e16cd6
FurkanYagciiii <bdonewac@outlook.com>
2025-01-17 16:56:29 +0300

    Added example.mp4 using Git LFS

--- Backend/obj/Debug/net8.0/UserAuthAPI.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/UserAuthAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserAuthAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1da877539af0efb0b8def77ab668d4de56eeded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e22490d16fbd6e41e1ab045eb679da6b5bb6f02d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserAuthAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserAuthAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
